59ff5366abaf8238e6043abd06103b189795725d
cyper <345343747@qq.com>
2015-11-24 19:17:04 +0800

    add the mustache module

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-define(['jquery','underscore'], function ($, _) {
+define(['jquery','underscore', 'mustache'], function ($, _, Mustache) {
     var initialize = function () {
         console.log('hello, $ is ', $.fn.jquery);
         console.log('hello, _ is ', _.VERSION);
+
+        var person = {
+            firstName: 'Cyper',
+            lastName: 'Yin',
+            blogURL: 'http://cyper.me'
+        };
+
+        var template = "<h1>{{firstName}} {{lastName}}</h1>Blog: {{blogURL}}";
+        var html = Mustache.to_html(template, person);
+        $("#sampleArea").html(html);
+
     };
 
     return {
--- main.js
+++ main.js
@@ -2,7 +2,9 @@ require.config({
     baseUrl: 'lib/',
     paths: {
         jquery: 'jquery-2.1.4',
-        underscore: 'underscore-1.8.3'
+        underscore: 'underscore-1.8.3',
+        mustache: 'mustache-2.2.0',
+        templates: '../templates'
     },
     shim: {
         // nothing
