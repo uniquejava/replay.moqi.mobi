ba8acfe3612eaf43308a78691c305ff680f50471
cyper <345343747@qq.com>
2015-11-26 04:11:17 +0800

    change the UI

--- js/BlogPostView.js
+++ js/BlogPostView.js
@@ -2,7 +2,7 @@ define(['jquery',
         'underscore',
         'backbone',
         'mustache',
-        'text!templates/blog_posts.html',
+        'text!templates/blog_posts_lists.html',
         'json!http://localhost:3000/blogs'
     ],
     function ($, _, Backbone, Mustache, blogsTemplate, blogPosts) {
@@ -11,13 +11,19 @@ define(['jquery',
 
             render: function () {
                 var blogs = [];
+
+                var parseDate = function(dateTime){
+                    var date = new Date(dateTime);
+                    return date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日';
+                };
+
                 $.each(blogPosts, function (key, val) {
                     var blog = {};
                     blog.title = val['title'];
                     blog.description = val['description'];
                     blog.slug = val['slug'];
                     blog.keywords = val['keywords'];
-                    blog.created = val['created'];
+                    blog.created = parseDate(val['created']);
                     blogs.push(blog);
                 });
 
