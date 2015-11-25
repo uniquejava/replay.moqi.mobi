8d87aa6ebbfc333027cd0742b5fe11006b8c1eb5
cyper <345343747@qq.com>
2015-11-25 14:04:59 +0800

    beautiful the ui and add the blog test

--- js/app.js
+++ js/app.js
@@ -8,8 +8,32 @@ define(['jquery','underscore', 'mustache', 'text!templates/nav.html'], function 
             firstName: 'Cyper',
             lastName: 'Yin',
             blogURL: 'http://cyper.me',
-            run: "python -m SimpleHTTPServer 8000"
+            run: "python -m SimpleHTTPServer 8000",
+            title: "This is a blog test",
+            description: "Is there a way to take the JSON from a $.getJSON() call and assign it",
+            slug: 'http://www.cyper.me',
+            keywords: "python -m SimpleHTTPServer 8000",
+            created: "2015-11-25"
+
         };
+
+        var blog_posts = [];
+        var title = [];
+        var description = [];
+        var slug = [];
+        var keywords = [];
+        var created = [];
+
+        /*
+        $.getJSON("url", {}, function(data) {
+            $.each(data, function (key,val) {
+                title.push(val['title']);
+                description.push(val['description']);
+                slug.push(val['slug']);
+                keywords.push(val[keywords]);
+                created.push(val['created']);
+            });
+        })*/
 
         var template = navTemplate;
         var html = Mustache.to_html(template, info);
