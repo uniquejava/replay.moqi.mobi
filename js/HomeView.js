79da08eb37cd5a18e6b00136ba3b4bc20b43e747
cyper <345343747@qq.com>
2015-11-25 20:58:37 +0800

    fix the products error & footer

--- js/HomeView.js
+++ js/HomeView.js
@@ -2,17 +2,24 @@ define(['jquery',
         'underscore',
         'backbone',
         'mustache',
+        'text!../index.html',
+        'json!../configure.json',
         '../js/BlogPostView',
         '../js/ProductsView',
     ],
-    function ($, _, Backbone, Mustache, BlogPostView, ProductsView) {
+    function ($, _, Backbone, Mustache, indexTemplate, config, BlogPostView, ProductsView) {
         var HomeView = Backbone.View.extend({
+            el: $('head'),
+
             render: function () {
                 var blogPostView = new BlogPostView();
                 blogPostView.render();
 
                 var productsView = new ProductsView();
                 productsView.render();
+
+                var html = Mustache.to_html(indexTemplate, config['seoinfo']);
+                this.$el.html(html)
 
             }
         });
